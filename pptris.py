4381bed334f92b4cb955103b1e1d47e7709fa570
David Boxwell <davidboxwell@ymail.com>
2020-12-27 22:56:46 +0000

    Tidy and timing things

--- pptris.py
+++ pptris.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
+
+#^^shebang for easy command lining^^
+
 import tkinter as tk
 import random
+
+#set up global constants
+PLAYFIELD_WIDTH = 10
+PLAYFIELD_HEIGHT = 20
 
 blooks = [
     [[1,1],
@@ -31,32 +39,86 @@ blooks = [
      [0,0,0]]
 ]
 
-class Blook:
-    def init(self, content = []):
-        self.segments = content
+defaultPositions = [[4,0], [3,0], [3,0], [3,0], [3,0], [3,0], [3,0]]
 
-    def print(self):
+tileColours = ["black", "white"]
+#root needs to be global for some methods idk the best way to do this
+scene = None
+
+class Blook:
+    def __init__(self, content, pos):
+        self.segments = content
+        self.position = pos
+
+    def __str__(self):
+        out = ""
         for s in self.segments:
-            print(s)
+            out += str(s) + "\n"
+        return out[:-1]
 
     def rotateClockwise(self):
+        #construct new array with [x,y] equal to old array [y, width - x - 1] (this is equivalent to a rotation about its centre)
         self.segments = [[self.segments[len(self.segments) - i - 1][j] for i in range(len(self.segments))] for j in range(len(self.segments[0]))]
 
     def rotateAClockwise(self):
+        #construct new array with [x,y] equal to old array [width - y - 1, x] (this is equivalent to a rotation about its centre)
         self.segments = [[self.segments[i][len(self.segments[0]) - 1 - j] for i in range(len(self.segments))] for j in range(len(self.segments[0]))]
+    
+    def draw(self, canvas):
+        for i in range(len(self.segments)):
+            for j in range(len(self.segments[i])):
+                if self.segments[j][i]:
+                    canvas.create_rectangle((self.position[0] + i) * 20 + 10, (self.position[1] + j) * 20 + 10,
+                                            (self.position[0] + i) * 20 + 30, (self.position[1] + j) * 20 + 30, fill="white", tag='blook')
 
-b = Blook(blooks[random.randint(0, 6)])
+class Scene:
+    def __init__(self):
+        self.frames = 0
+        
+        self.playfield = [[0 for i in range(PLAYFIELD_WIDTH)] for j in range(PLAYFIELD_HEIGHT)] # 2D array 20x10
+        self.selBlook = makeRandomBlock()
+        self.storedBlook = None
+        self.nextBlook = None
 
-sel_blook = None
-stored_blook = None
-next_blook = None
+        self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root, width=320, height=480)
+        self.canvas.pack()
 
-playfield = [[0 for i in range(10)] for j in range(20)] # 2D array 20x10
+        self.root.bind('<Left>', leftKey)
+        self.root.bind('<Right>', rightKey)
+        self.root.bind('<Up>', upKey)
+        self.root.bind('<Down>', downKey)
+        
+    def start(self):
+        self.root.after(20, update)
+        self.root.mainloop()
 
-root = tk.Tk()
+    def update(self):
+        self.frames = (self.frames + 1) % 50
+        
+        if self.frames == 0:
+            self.selBlook.position[1] += 1
+        
+        #print("update")
+        #print(self.selBlook)
+    def draw(self):
+        self.draw_playfield()
+        self.selBlook.draw(self.canvas)
+        self.canvas.update()
+    def draw_playfield(self):
+        self.canvas.delete('playfield')
+        for i in range(PLAYFIELD_HEIGHT):#draw to the size of the play field not random constants
+            col = None
+            for j in range(PLAYFIELD_WIDTH):
+                #use tileColours array too choose the colour for the tile (branchless programming techiques boi)
+                self.canvas.create_rectangle(j * 20 + 10, i * 20 + 10,
+                                             j * 20 + 30, i * 20 + 30,
+                                             fill=tileColours[self.playfield[i][j]], tag='playfield')
 
-canvas = tk.Canvas(root, width=320, height=480)
-canvas.pack()
+
+def makeRandomBlock():
+    index = random.randint(0, 6)
+    return Blook(blooks[index], defaultPositions[index])
 
 def leftKey(event):
     None
@@ -70,30 +132,15 @@ def upKey(event):
 def downKey(event):
     None
 
-root.bind('<Left>', leftKey)
-root.bind('<Right>', rightKey)
-root.bind('<Up>', upKey)
-root.bind('<Down>', downKey)
-
-def draw_playfield():
-    canvas.delete('playfield')
-    for i in range(20):
-        col = None
-        for j in range(10):
-            if playfield[i][j] == 1:
-                col = 'black'
-            else:
-                col = 'white'
-            canvas.create_rectangle(j * 20 + 10, i * 20 + 10, j * 20 + 30, i * 20 + 30, fill=col, tag='playfield')
-    canvas.update()
-
 def update():
-    draw_playfield()
-    root.after(20, update)    
+    scene.update()
+    scene.draw()
+    scene.root.after(20, update)  
 
-root.after(20, update)
-
-root.mainloop()
+#iff thing for easier unit testing
+if __name__ == "__main__":
+    scene = Scene()
+    scene.start()
 
 
 
