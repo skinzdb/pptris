7ff44d7a52c1045f8d7ce3ba338ddc136be7dc85
David Boxwell <davidboxwell@ymail.com>
2020-12-30 18:44:18 +0000

    Setup some variables

--- pptris.c
+++ pptris.c
@@ -1,12 +1,19 @@
+#include <stdint.h>
 
 #define PLAYFIELD_WIDTH 10
 #define PLAYFIELD_HEIGHT 20
 
 typedef struct {
-    int** segments;
+    uint_fast16_t* segments;
     int x;
     int y;
 } Block;
+
+uint_fast16_t* playfield[PLAYFIELD_HEIGHT];
+
+Block sel_block;
+Block next_block;
+Block stored_block;
 
 int main() {
 
