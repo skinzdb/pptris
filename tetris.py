2b40645b292fa0bbc4aa88c3c0f3bc13e9fe8da5
David Boxwell <davidboxwell@ymail.com>
2022-09-15 22:36:33 +0100

    Start of colours

--- tetris.py
+++ tetris.py
@@ -7,19 +7,35 @@ screen = pygame.display.set_mode([400, 600])
 
 running = True
 
+GRID_WIDTH = 10
+GRID_HEIGHT = 24
+START_POS = (3, 0)
+
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+ORANGE = (255,165,0)
+YELLOW = (255,255,0)
+GREEN = (0,255,0)
+L_BLUE = (0,255,255)
+D_BLUE = (0,0,255)
+PURPLE = (128,0,128)
+
+class Tile:
+    def __init__(self, colour):
+        self.colour = colour
+        self.is_baked = False
+    def bake(self):
+        self.is_baked = True
+
 tetronimoes = [
-    [0x0330, 0x0330, 0x0330, 0x0330], # O
-    [0x02D0, 0x0446, 0x00D8, 0x0622], # L
-    [0x08D0, 0x0644, 0x00D2, 0x0226], # J
+    [0x0660, 0x0660, 0x0660, 0x0660], # O
+    [0x02E0, 0x0446, 0x00E8, 0x0622], # L
+    [0x08E0, 0x0644, 0x00E2, 0x0226], # J
     [0x0F00, 0x2222, 0x00F0, 0x4444], # I
     [0x06C0, 0x0462, 0x006C, 0x08C4], # S
     [0x0C60, 0x0264, 0x00C6, 0x04C8], # Z
-    [0x04D0, 0x0464, 0x00D4, 0x04C4]  # T
+    [0x04E0, 0x0464, 0x00E4, 0x04C4]  # T
 ]
-
-GRID_WIDTH = 10
-GRID_HEIGHT = 24
-START_POS = (4, 0)
 
 curr_tetronimo = None
 held_tetronimo = None
@@ -28,18 +44,33 @@ xoff = 0
 yoff = 0
 rotation = 0
 
-grid = [0] * GRID_HEIGHT
+grid = []
+
+for j in range(GRID_HEIGHT):
+    row = []
+    for i in range(GRID_WIDTH):
+        row.append(Tile(BLACK))
+    grid.append(row)
 
 def draw(curr):
     pass
 
 def add():
-    tetronimo = random.choice(tetronimoes)
-    for i in range(4):
-        grid[i] |= tetronimo[0] << START_POS[0]
+    for row in grid:
+        for i in range(len(row)):
+            row[i] = Tile(BLACK)
 
-    print(grid)
+    xoff = START_POS[0]
+    yoff = START_POS[1] 
+
+    tetronimo = random.choice(tetronimoes)
+    for j in range(4):
+        for i in range(4):
+            is_full = tetronimo[0]
+            grid[j + yoff][i + xoff] = (tetronimo[0] >> (12 - 4 * i) & 0xF) << (7 - START_POS[0])
     
+    print(grid)
+
 
 def update():
     for i in range(4):
@@ -48,24 +79,28 @@ def update():
             if yoff + 4 - i >= GRID_HEIGHT:
                 continue
             
-
             pass
 
 frames = 0
-
-add()
+update_speed = 30 # number of frames before update
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    screen.fill((144, 144, 144))
+    add()
 
-    for j in range(GRID_HEIGHT):
+    if frames >= update_speed:
+        update()
+        frames = 0
+
+    for j in range(GRID_HEIGHT - 2):
         for i in range(GRID_WIDTH):
-
-            pygame.draw.rect(screen, (20,20,20), pygame.Rect(i * 5, j * 5, 5, 5))
+            isFull = (grid[j + 2] & (1 << (GRID_WIDTH - i))) > 0 
+            colour = (255, 255, 255) if isFull else (0, 0, 0) 
+            pygame.draw.rect(screen, colour, pygame.Rect(i * 25, j * 25, 25, 25))
 
     pygame.display.flip()
     
